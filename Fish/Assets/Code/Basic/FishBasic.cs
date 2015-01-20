26a6b06774b65f5ddc8f75c5a991ace02d59058d
jweimann <jasonweimann@gmail.com>
2015-01-19 22:08:52 -0800

    Fish

---
+++ Fish/Assets/Code/Basic/FishBasic.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishBasic : MonoBehaviour
+{
+	[SerializeField] // This makes it show in the Inspector
+    private float _flightHeight = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            rigidbody2D.velocity = Vector2.zero;
+			rigidbody2D.AddForce(Vector3.up * _flightHeight);
+        }
+
+        if (transform.position.y < -19f ||
+         transform.position.y > 25f)
+        {
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+    }
+}
---
+++ Fish/Assets/Code/Basic/SeaweedBasic.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeaweedBasic : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3f;
+    [SerializeField]
+    private float _screenEdge = 15f;
+
+	// Update is called once per frame
+    void Update()
+    {
+        MoveForward();
+
+        if (IsOffLeftSideOfScreen())
+        {
+            MoveToRightOfScreen();
+        }
+    }
+
+    private void MoveForward()
+    {
+        this.transform.Translate(Vector3.left * Time.deltaTime * _speed);
+    }
+
+	bool IsOffLeftSideOfScreen ()
+	{
+		return this.transform.position.x < -_screenEdge;
+	}
+    
+    private void MoveToRightOfScreen()
+    {
+        this.transform.position = new Vector3(_screenEdge, this.transform.position.y, 0f);
+    }
+}
